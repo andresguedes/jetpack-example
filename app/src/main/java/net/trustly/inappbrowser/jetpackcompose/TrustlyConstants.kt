b7fd33f82c7a9a91866f5ec102a58d74701ea867
Andre Guedes <andre.guedes@trustly.com>
2025-02-21 19:37:13 -0300

    Changing some constants value

--- app/src/main/java/net/trustly/inappbrowser/jetpackcompose/TrustlyConstants.kt
+++ app/src/main/java/net/trustly/inappbrowser/jetpackcompose/TrustlyConstants.kt
@@ -2,8 +2,8 @@ package net.trustly.inappbrowser.jetpackcompose
 
 object TrustlyConstants {
 
-    const val TRUSTLY_URL = "http://10.0.2.2:3000?integrationContext=InAppBrowser&urlScheme="
+    const val TRUSTLY_URL = "http://localhost:3000?integrationContext=InAppBrowser&urlScheme="
     const val OAUTH_LOGIN_PATH = "/oauth/login"
-    const val TRUSTLY_PROCEED_TO_CHOOSE_ACCOUNT_SCRIPT = "javascript:if (window.Trustly && window.Trustly.proceedToChooseAccount) window.Trustly.proceedToChooseAccount();"
+    const val TRUSTLY_PROCEED_TO_CHOOSE_ACCOUNT_SCRIPT = "javascript:window.Trustly.proceedToChooseAccount();"
 
 }
